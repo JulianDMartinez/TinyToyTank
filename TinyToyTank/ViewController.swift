3e9e265a2d5d3df7bf98336c079053fe6834553d
Julian Martinez <julian.martinez.s@outlook.com>
2021-11-21 16:13:26 -0500

    Created project and connected UIKit components to Reality Composer Animations

--- TinyToyTank/ViewController.swift
+++ TinyToyTank/ViewController.swift
@@ -10,15 +10,67 @@ import RealityKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet var arView: ARView!
+    var tankAnchor: TinyToyTank._TinyToyTank?
+    
+    @IBOutlet weak var arView: ARView!
+    
+    @IBOutlet weak var tankLeftButton   : UIButton!
+    @IBOutlet weak var tankForwardButton: UIButton!
+    @IBOutlet weak var tankRightButton  : UIButton!
+    @IBOutlet weak var turretLeftButton : UIButton!
+    @IBOutlet weak var cannonFireButton : UIButton!
+    @IBOutlet weak var turretRightButton: UIButton!
+    
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+        do {
+            tankAnchor = try TinyToyTank.load_TinyToyTank()
+            tankAnchor?.cannon?.setParent(tankAnchor?.tank, preservingWorldTransform: true)
+        } catch {
+            debugPrint("There was an error while attempting to access the tinty toytank scene.\nError: \(error)")
+        }
+        
+
         
         // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        if let tankAnchor = tankAnchor {
+            arView.scene.anchors.append(tankAnchor)
+        }
     }
+    
+    
+    @IBAction func tankRightPressed(_ sender: UIButton) {
+        tankAnchor!.notifications.tankRight.post()
+    }
+    
+    
+    @IBAction func tankForwardPressed(_ sender: UIButton) {
+        tankAnchor!.notifications.tankForward.post()
+    }
+    
+    
+    @IBAction func tankLeftPressed(_ sender: UIButton) {
+        tankAnchor!.notifications.tankLeft.post()
+    }
+    
+    
+    @IBAction func turretLeftPressed(_ sender: UIButton) {
+        tankAnchor!.notifications.turretLeft.post()
+    }
+    
+    
+    @IBAction func cannonFirePressed(_ sender: UIButton) {
+        tankAnchor!.notifications.cannonFire.post()
+    }
+    
+    
+    @IBAction func turretRightPressed(_ sender: UIButton) {
+        tankAnchor!.notifications.turretLeft.post()
+    }
+    
+    
 }
